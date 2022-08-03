c4869854b848171e73998444ba2f268e96a20c96
Peter D. Barnes, Jr <barnes26@llnl.gov>
2022-08-03 12:05:28 -0700

    profile: avoid divide by zero failure

--- src/sst/core/profile/syncProfileTool.cc
+++ src/sst/core/profile/syncProfileTool.cc
@@ -57,7 +57,8 @@ SyncProfileToolTime<T>::outputData(FILE* fp)
     fprintf(fp, "%s (id = %" PRIu64 ")\n", name.c_str(), my_id);
     fprintf(fp, "  SyncManager Count = %" PRIu64 "\n", syncmanager_count);
     fprintf(fp, "  Total SyncManager Time = %lfs\n", (float)syncmanager_time / 1000000000.0);
-    fprintf(fp, "  Average SyncManager Time = %" PRIu64 "ns\n", syncmanager_time / syncmanager_count);
+    fprintf(fp, "  Average SyncManager Time = %" PRIu64 "ns\n", 
+	    syncmanager_time == 0 ? 0 : syncmanager_time / syncmanager_count);
 }
 
 
